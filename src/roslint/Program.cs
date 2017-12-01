98d959f3cf7da81a5f8916f66a47c93c2a0857a2
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-12-01 11:03:34 -0800

    initial template

---
+++ src/roslint/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace roslint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
